4e32b15fe6d91a55ab6fb14e0f78b0dee5976ebd
Mohammed <faisalshahna38@gmail.com>
2023-09-23 00:26:56 +0530

    some commented js code for navbar animation

--- function.js
+++ function.js
@@ -7,3 +7,17 @@ const myself = {
 
 document.getElementById("myname").innerHTML = myself.firstName + " " + myself.midName + " " + myself.lastName;
 document.getElementById("mypost").innerHTML = myself.myPost;
+
+const pid = getElementById("projects");
+const navclass = getElementByClassName("nav");
+
+
+// window.addEventListener('scroll', function() {
+//     let nav = document.querySelector(navclass);
+//     if(window.pageYOffset > 0){
+//         nav.classList.add('navsc');
+//     }
+//     else{
+//         nav.classList.remove('navsc');
+//     }
+// })
