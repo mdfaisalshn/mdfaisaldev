530ec80f388081fa17ff90563f3d5901403b5387
Mohammed <faisalshahna38@gmail.com>
2023-09-23 18:28:18 +0530

    projects page added

--- function.js
+++ function.js
@@ -1,16 +1,6 @@
 const myself = {
-    firstName : "Mohammed",
-    midName : "Faisal",
-    lastName : "Shahna",
-    myPost : "Web" + " " + "Designer"
+    myPost : "Web" + " " + "Designer",
 }
-
-document.getElementById("myname").innerHTML = myself.firstName + " " + myself.midName + " " + myself.lastName;
-document.getElementById("mypost").innerHTML = myself.myPost;
-
-const pid = getElementById("projects");
-const navclass = getElementByClassName("nav");
-
 
 // window.addEventListener('scroll', function() {
 //     let nav = document.querySelector(navclass);
@@ -18,6 +8,6 @@ const navclass = getElementByClassName("nav");
 //         nav.classList.add('navsc');
 //     }
 //     else{
-//         nav.classList.remove('navsc');
+//         nav.cla  ssList.remove('navsc');
 //     }
 // })
