20b9abbebbc32be9cc63a886c308c719d8722910
Mohammed <faisalshahna38@gmail.com>
2023-09-24 00:24:31 +0530

    added animation

--- function.js
+++ function.js
@@ -1,13 +1,33 @@
-const myself = {
-    myPost : "Web" + " " + "Designer",
+let mypost = document.querySelector('#mypost');
+let post = "Designer";
+let flag = 0;
+
+let index = 0;
+
+const typeWriter = () => {
+    let new_post = post.slice(0,index);
+    new_post += "_";
+    mypost.innerText = new_post;
+    
+    if (flag===0) {
+        index++;
+    }
+    else if (flag===1) {
+        index--;
+    }
+    if(index > post.length) {
+        flag = 1;
+    }
+    if (index === 0 && post==="Designer") {
+        flag = 0;
+        post = "Developer";
+    }
+    else if (index === 0 && post==="Developer") {
+        flag = 0;
+        post = "Designer";
+    }
+
+    setTimeout(() => typeWriter(), 150);
 }
 
-// window.addEventListener('scroll', function() {
-//     let nav = document.querySelector(navclass);
-//     if(window.pageYOffset > 0){
-//         nav.classList.add('navsc');
-//     }
-//     else{
-//         nav.cla  ssList.remove('navsc');
-//     }
-// })
+typeWriter();
