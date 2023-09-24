08f89cc5d12e129ece48dcc587ccdbc9b17fba9e
Mohammed <faisalshahna38@gmail.com>
2023-09-24 17:25:43 +0530

    Updated gsap js animation

--- function.js
+++ function.js
@@ -1,3 +1,17 @@
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
+
 let mypost = document.querySelector('#mypost');
 let post = "Designer";
 let flag = 0;
@@ -42,22 +56,22 @@ function animation(){
     tl.from("#nav",{
         y: '-10',
         opacity: 0,
-        duration: 0.3,
+        duration: 1,
         ease: Expo.easeInOut
     })
     .from("#hello",{
         y: '50',
         opacity: 0,
         ease: Expo.easeInOut,
-        duration: 0.5,
-        delay: 0,
+        duration: 1,
+        delay: -1,
         stagger: .2
     })
     .from("#myname",{
         y: '50',
         opacity: 0,
         ease: Expo.easeInOut,
-        duration: 0.5,
+        duration: 0.3,
         delay: 0,
         stagger: .2
     })
@@ -65,7 +79,7 @@ function animation(){
         y: '50',
         opacity: 0,
         ease: Expo.easeInOut,
-        duration: 0.5,
+        duration: 0.3,
         delay: 0,
         stagger: .2
     })
@@ -81,18 +95,245 @@ function animation(){
 
 animation();
 
-// let tl = gsap.timeline({
-//     scrollTrigger: {
-//         trigger: '.sec-6',
-//         start: '0% center',
-//         end: '10% center',
-//         scrub: true,
-//         markers: false
-//     }
-// })
+let tl1 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.sec-6',
+        start: '15% 80%',
+        end: '35% 15%',
+        toggleAction: "restart reverse",
+        scrub: false,
+        markers: false
+    }
+})
 
-// tl.from('.sec-6', {
-//     x: -10,
-//     opacity: 0,
-//     ease: Expo.easeInOut
-// })
+tl1.from('#proj-head', {
+    x: -10,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl1.from('.card-1', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl1.from('.card-2', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl1.from('.card-3', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl1.from('.card-4', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl1.from('.card-5', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl1.from('.card-6', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+let tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.sec-2',
+        start: '10% 80%',
+        end: '35% 20%',
+        scrub: false,
+        markers: false
+    }
+})
+
+tl2.from('#resume-head', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl2.from('.education', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl2.from('.skills', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl2.from('.front-end', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl2.from('.prog-langs', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl2.from('.back-end', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl2.from('.speak-langs', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+let tl3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.sec-3',
+        start: '15% 80%',
+        end: '35% 20%',
+        scrub: false,
+        markers: false
+    }
+})
+
+tl3.from('#cont-head', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl3.from('.inp-1', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl3.from('.inp-2', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl3.from('.inp-3', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl3.from('#feed-mssg', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl3.from('#mssg-span', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+tl3.from('.inp-4', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl3.from('#form-btn', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+let tl5 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.sec-5',
+        start: '20% 80%',
+        end: '35% 20%',
+        scrub: false,
+        markers: false
+    }
+})
+
+tl5.from('#abt-head', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl5.from('.img', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl5.from('.about p', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl5.from('.cb-1', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl5.from('.cb-2', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl5.from('.cb-3', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
+
+tl5.from('.cb-4', {
+    x: -50,
+    opacity: 0,
+    ease: Expo.easeInOut,
+    duration: 0.1
+})
