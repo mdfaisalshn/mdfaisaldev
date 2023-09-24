577eb575c1a91275697939723f507cdab5b62edb
Mohammed <faisalshahna38@gmail.com>
2023-09-24 20:17:43 +0530

    added counter animation  with js

--- function.js
+++ function.js
@@ -337,3 +337,40 @@ tl5.from('.cb-4', {
     ease: Expo.easeInOut,
     duration: 0.1
 })
+
+let counts1 = setInterval(updated1);
+        let upto1 = 0;
+        function updated1() {
+            let count1 = document.getElementById("counter-1");
+            count1.innerHTML = ++upto1;
+            if (upto1 === 100) {
+                clearInterval(counts1);
+            }
+        }
+let counts2 = setInterval(updated2);
+        let upto2 = 0;
+        function updated2() {
+            let count2 = document.getElementById("counter-2");
+            count2.innerHTML = ++upto2;
+            if (upto2 === 100) {
+                clearInterval(counts2);
+            }
+        }
+let counts3 = setInterval(updated3);
+        let upto3 = 0;
+        function updated3() {
+            let count3 = document.getElementById("counter-3");
+            count3.innerHTML = ++upto3;
+            if (upto3 === 250) {
+                clearInterval(counts3);
+            }
+        }
+let counts4 = setInterval(updated4);
+        let upto4 = 0;
+        function updated4() {
+            let count4 = document.getElementById("counter-4");
+            count4.innerHTML = ++upto4;
+            if (upto4 === 450) {
+                clearInterval(counts4);
+            }
+        }
